357938d8df0edf0ddf5949d1393c318b0462c566
takano630 <t.takano@4nonome.com>
2021-12-30 17:30:42 +0900

    first commit

---
+++ twitter/views.py
@@ -0,0 +1,15 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import Accountform
+from django.views.generic import TemplateView
+
+class Start(TemplateView):
+  template_name = 'twitter_HTML/start.html'
+
+class Signup(CreateView):
+    form_class = Accountform
+    template_name = "twitter_HTML/formpage.html" 
+    def form_valid(self, form):
+      user = form.save() 
+      return render(self.request,'twitter_HTML/signup.html') 
